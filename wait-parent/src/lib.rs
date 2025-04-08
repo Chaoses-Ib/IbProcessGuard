a7c8467020c61b206abe92d7d790b11016809a24
Chaoses-Ib <Chaos-es@outlook.com>
2025-04-08 15:11:04 +0800

    feat(wait-parent): add `debug` feature

--- wait-parent/src/lib.rs
+++ wait-parent/src/lib.rs
@@ -1,5 +1,21 @@
 use std::process::ExitStatus;
 
+#[cfg(feature = "debug")]
+#[doc(hidden)]
+#[allow(unused)]
+macro_rules! sysinfo_debug {
+    ($($x:tt)*) => {{
+        eprintln!($($x)*);
+    }}
+}
+
+#[cfg(not(feature = "debug"))]
+#[doc(hidden)]
+#[allow(unused)]
+macro_rules! sysinfo_debug {
+    ($($x:tt)*) => {{}};
+}
+
 pub fn wait_parent() -> Option<ExitStatus> {
     let mut system = sysinfo::System::new();
     let current_pid = sysinfo::get_current_pid().unwrap();
@@ -9,6 +25,7 @@ pub fn wait_parent() -> Option<ExitStatus> {
         sysinfo::ProcessRefreshKind::nothing(),
     );
     let parent_pid = system.process(current_pid).unwrap().parent().unwrap();
+    sysinfo_debug!("parent_pid: {:?}", parent_pid);
     system.refresh_processes_specifics(
         sysinfo::ProcessesToUpdate::Some(&[parent_pid]),
         false,
@@ -16,7 +33,10 @@ pub fn wait_parent() -> Option<ExitStatus> {
     );
     match system.process(parent_pid) {
         Some(parent) => parent.wait(),
-        None => None,
+        None => {
+            sysinfo_debug!("parent process not found: {:?}", parent_pid);
+            None
+        }
     }
 }
 
